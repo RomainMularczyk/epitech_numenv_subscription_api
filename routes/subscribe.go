4e34133c5b58d21eec3fe0b332c6d7961f273aed
Romain Mularczyk <romain.mularczyk@gmail.com>
2023-12-30 16:03:53 +0100

    [MOD] group POST /subscribe route

--- routes/subscribe.go
+++ routes/subscribe.go
@@ -2,12 +2,16 @@ package routes
 
 import (
 	"numenv_subscription_api/controllers"
+	"numenv_subscription_api/middlewares"
 
 	"github.com/labstack/echo/v4"
 )
 
 func Subscribe(e *echo.Echo) {
-	e.POST("/subscribe/:sessionName", controllers.Subscribe)
-	e.GET("/subscribers", controllers.ReadAll)
+	e.GET("/subscribe", controllers.ReadAll)
+  // Groups
+  g := e.Group("/subscribe")
+  g.Use(middlewares.IsSessionFull)
+	g.POST("/:speaker", controllers.Subscribe, middlewares.IsSessionFull)
 }
 
