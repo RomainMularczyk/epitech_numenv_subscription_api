d5d32a600da2e3a6ea779c360f10feb81bc62be8
Romain Mularczyk <romain.mularczyk@gmail.com>
2023-12-30 10:19:17 +0100

    [ADD] discord client connection

--- services/discord/discord_client.go
+++ services/discord/discord_client.go
@@ -1,23 +1,49 @@
 package discord
 
 import (
-	"numenv_subscription_api/errors/logs"
 	"os"
+	"os/signal"
+	"numenv_subscription_api/errors/logs"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func DiscordClient() {
-  discordClient, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+func DiscordClient() error {
+  // Provide Discord bot connection token
+  session, err := discordgo.New(
+    "Bot " + os.Getenv("DISCORD_BOT_TOKEN"),
+  )
   if err != nil {
     logs.Output(
       logs.ERROR,
       "Could not connect to Discord server.",
     )
+    return err
   }
 
-  discordClient.Identify.Intents = discordgo.IntentsAll
+  // Providing Discord bot permissions
+  session.Identify.Intents = discordgo.IntentsAll
 
-  discordClient.GuildMemberRoleAdd("1175146908965159013", "236577940651900928", "1188650394335842344")
+  // Connection Discord bot
+  err = session.Open()
+  if err != nil {
+    logs.Output(
+      logs.ERROR,
+      "Could not connect Discord bot.",
+    )
+    return err
+  }
+  logs.Output(logs.INFO, "Discord bot connected.")
 
+  // Adding Discord bot commands
+  DiscordUserRegistrationCommand(session)
+
+  // Catching Ctrl + C signal to shutdown Discord bot
+  logs.Output(logs.INFO, "Press Ctrl+C to stop Discord bot.")
+  stop := make(chan os.Signal, 1)
+  signal.Notify(stop, os.Interrupt)
+  <- stop
+  
+
+  return nil
 }
