15f5f281a738261c4e9d8576103b0580cd0755e7
Romain Mularczyk <romain.mularczyk@gmail.com>
2023-12-30 10:20:17 +0100

    [DEL] discord client call

--- services/subscribe.go
+++ services/subscribe.go
@@ -5,24 +5,23 @@ import (
 	"numenv_subscription_api/repositories"
   "numenv_subscription_api/errors/logs"
 	"numenv_subscription_api/services/mail"
-  "numenv_subscription_api/services/discord"
 
-	"github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4"
   "github.com/google/uuid"
 )
 
+// Register a user to the database and creates a unique string 
+// that is used to also register the same user on Discord so 
+// it can access the corresponding channel
 func Subscribe(
   c echo.Context, 
   user *models.Subscriber, 
   speaker string,
 ) error {
-  discord.DiscordClient()
-  sess, err := repositories.GetSessionBySpeaker(c.Request().Context(), speaker)
-
-	err = repositories.Subscribe(c.Request().Context(), user, sess.Id)
-	if err != nil {
-		return err
-	}
+  sess, err := repositories.GetSessionBySpeaker(
+    c.Request().Context(), 
+    speaker,
+  )
 
   uniqueStr, err := uuid.NewRandom()
   if err != nil {
@@ -32,10 +31,22 @@ func Subscribe(
     )
   }
 
+	err = repositories.Subscribe(
+    c.Request().Context(), 
+    user,
+    sess.Id,
+    uniqueStr.String(),
+  )
+	if err != nil {
+		return err
+	}
+
   mail.SendMail(user.Email, sess.Name, uniqueStr.String())
+
 	return nil
 }
 
+// Get all the subscribers
 func ReadAll(c echo.Context) ([]*models.Subscriber, error) {
 	result, err := repositories.ReadAll(c.Request().Context())
 	if err != nil {
