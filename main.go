843475ab1e4a8ec31d231a6d1605e3d991aac405
Romain Mularczyk <romain.mularczyk@gmail.com>
2023-12-30 10:16:54 +0100

    [MOD] run bot + server in parallel

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"numenv_subscription_api/routes"
+	"numenv_subscription_api/services/discord"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+  defer discord.DiscordClient()
+
+  // Starting server
 	e := echo.New()
 	routes.Subscribe(e)
-
-	e.Logger.Fatal(e.Start(":1323"))
+  
+	go func() {
+    e.Logger.Fatal(e.Start(":1323"))
+  }()
 }
 
