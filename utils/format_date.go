4d5ddb7aaae1c3699992ab1ecea396cfe6d6b100
myriam-tmds <myriam@tmds.io>
2024-01-01 19:30:08 +0100

    [MOD] format date fct

--- utils/format_date.go
+++ utils/format_date.go
@@ -5,14 +5,13 @@ import (
 	"time"
 )
 
-func FormatDate(date string) time.Time {
-  date, err := time.Parse(time.RFC3339, date)
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      "Could not parse the date.",
-    )
-  }
-
-  return date.Format("DD-MM-YYYY")
+func FormatDate(date string) string {
+	parsedDate, err := time.Parse(time.RFC3339, date)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			"Could not parse the date.",
+		)
+	}
+	return parsedDate.Format("02-01-2006")
 }
