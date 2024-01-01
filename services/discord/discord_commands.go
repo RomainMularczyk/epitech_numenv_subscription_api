d58b0d43313a0bca3680a9798815f1043c69a49c
myriam-tmds <myriam@tmds.io>
2024-01-01 19:32:48 +0100

    [ADD] new discord command subscribe to session

--- services/discord/discord_commands.go
+++ services/discord/discord_commands.go
@@ -8,60 +8,73 @@ import (
 )
 
 func DiscordUserRegistrationCommand(
-  discordClient *discordgo.Session,
+	discordClient *discordgo.Session,
 ) {
-  appCommands := []*discordgo.ApplicationCommand {
-    {
-      Name: "register",
-      Description: "Register a new subscriber.",
-      Options: []*discordgo.ApplicationCommandOption {
-        {
-          Name: "key",
-          Description: "The key provided to register to a session.",
-          Type: discordgo.ApplicationCommandOptionString,
-        },
-      },
-    },
-    {
-      Name: "sessions",
-      Description: "List all the sessions available.",
-    },
-    {
-      Name: "my-sessions",
-      Description: "List all the sessions that I'm subscribed to.",
-    },
-  }
+	appCommands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "register",
+			Description: "Register a new subscriber.",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "key",
+					Description: "The key provided to register to a session.",
+					Type:        discordgo.ApplicationCommandOptionString,
+				},
+			},
+		},
+		{
+			Name:        "sessions",
+			Description: "List all the sessions available.",
+		},
+		{
+			Name:        "my-sessions",
+			Description: "List all the sessions that I'm subscribed to.",
+		},
+		{
+			Name:        "subscribe",
+			Description: "Subscribe to session.",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "name",
+					Description: "The name of the session to subscribe to.",
+					Type:        discordgo.ApplicationCommandOptionString,
+				},
+			},
+		},
+	}
 
-  _, err := discordClient.ApplicationCommandBulkOverwrite(
-    os.Getenv("DISCORD_APP_ID"),
-    os.Getenv("DISCORD_GUILD_ID"),
-    appCommands,
-  )
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      "Could not create the Discord application commands.",
-    )
-  }
+	_, err := discordClient.ApplicationCommandBulkOverwrite(
+		os.Getenv("DISCORD_APP_ID"),
+		os.Getenv("DISCORD_GUILD_ID"),
+		appCommands,
+	)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			"Could not create the Discord application commands.",
+		)
+	}
 
-  discordClient.AddHandler(discordInteractionCallback)
+	discordClient.AddHandler(discordInteractionCallback)
 }
 
 // Callback function provided to the `.AddHandler` method
-// to trigger when a user uses the `/register` command
+// to trigger when a user uses the commands
 func discordInteractionCallback(
-  session *discordgo.Session, 
-  interaction *discordgo.InteractionCreate,
+	session *discordgo.Session,
+	interaction *discordgo.InteractionCreate,
 ) {
-  if interaction.Type == discordgo.InteractionApplicationCommand {
-    data := interaction.ApplicationCommandData()
-    switch data.Name {
-    case "register":
-      RegisterSubscriber(session, interaction)
-    case "sessions":
-      ListSessions(session, interaction) 
-    case "my-sessions":
-      ListMySessions(session, interaction)
-    }
-  }
+	if interaction.Type == discordgo.InteractionApplicationCommand {
+		data := interaction.ApplicationCommandData()
+		switch data.Name {
+		case "register":
+			RegisterSubscriber(session, interaction)
+		case "sessions":
+			ListSessions(session, interaction)
+		case "my-sessions":
+			ListMySessions(session, interaction)
+		case "subscribe":
+			SubscribeToSession(session, interaction)
+		}
+	}
 }
--- services/discord/discord_subscribe_to_session.go
+++ services/discord/discord_subscribe_to_session.go
@@ -1,88 +1,112 @@
 package discord
 
 import (
+	"context"
 	"fmt"
+	dbError "numenv_subscription_api/errors/db"
 	"numenv_subscription_api/errors/logs"
+	"numenv_subscription_api/models"
+	"numenv_subscription_api/repositories"
 	"numenv_subscription_api/services"
 	"os"
+	"reflect"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func RegisterSubscriber(
-  s *discordgo.Session,
-  i *discordgo.InteractionCreate,
+func SubscribeToSession(
+	s *discordgo.Session,
+	i *discordgo.InteractionCreate,
 ) {
-  options := i.ApplicationCommandData().Options
-  for _, opt := range options {
-    switch opt.Type {
-    case discordgo.ApplicationCommandOptionString:
-      
-      // Save Discord Id in the subscribers table
-      uniqueStr := opt.StringValue()
-      sess, err := services.RegisterDiscordId(
-        i.Member.User.ID,
-        uniqueStr,
-      )
-      if err != nil {
-        sessErr := s.InteractionRespond(
-          i.Interaction,
-          &discordgo.InteractionResponse {
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData {
-              Content: "Merci d'entrer votre clé communiquée par mail.",
-            },
-          },
-        )
-        if sessErr != nil {
-          logs.Output(
-            logs.ERROR,
-            "Could not initiate Discord bot session response.",
-          )
-        }
-        return
-      }
-      // Get subscriber by unique str
-      subscriber, err := services.GetSubscriberByUniqueStr(uniqueStr)
-      if err != nil {
-        return
-      }
-      // Bot response to user interaction
-      err = s.InteractionRespond(
-        i.Interaction,
-        &discordgo.InteractionResponse {
-          Type: discordgo.InteractionResponseChannelMessageWithSource,
-          Data: &discordgo.InteractionResponseData {
-            Content: fmt.Sprintf(
-              `Vous êtes inscrit à la session : **%s** - *%s*.
-Elle aura lieu le %s.`,
-              sess.Speaker, sess.Name, sess.Date,
-            ),
-          },
-        },
-      )
-      s.GuildMemberRoleAdd(
-        os.Getenv("DISCORD_GUILD_ID"),
-        i.Member.User.ID,
-        sess.DiscordRoleId,
-      )
-      s.GuildMemberNickname(
-        os.Getenv("DISCORD_GUILD_ID"),
-        i.Member.User.ID,
-        fmt.Sprintf(
-          "%s (%s%v)",
-          i.Member.User.Username,
-          subscriber.Firstname,
-          subscriber.Lastname[0],
-        ),
-      )
-      if err != nil {
-        fmt.Println(err)
-        logs.Output(
-          logs.ERROR,
-          "Could not initiate bot response.",
-        )
-      }
-    }
-  }
+	options := i.ApplicationCommandData().Options
+	for _, opt := range options {
+		switch opt.Type {
+		case discordgo.ApplicationCommandOptionString:
+			speaker := opt.StringValue()
+
+			// Initiate Discord bot session response
+			sessErr := s.InteractionRespond(
+				i.Interaction,
+				&discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+				},
+			)
+			if sessErr != nil {
+				logs.Output(
+					logs.ERROR,
+					"Could not initiate Discord bot session response.",
+				)
+				return
+			}
+
+			sess, err := RegisterSubscriberToNewSession(i.Member.User.ID, speaker)
+			if err != nil {
+				msg := "Une erreur est survenue en tentant de vous inscrire à la session."
+				if reflect.TypeOf(err) == reflect.TypeOf(dbError.AlreadyRegisteredError{}) {
+					msg = "Vous êtes déjà inscrit.e à cette session."
+				}
+				_, err := s.FollowupMessageCreate(
+					i.Interaction,
+					false,
+					&discordgo.WebhookParams{
+						Content: msg,
+					},
+				)
+				if err != nil {
+					return
+				}
+
+				return
+			}
+
+			err = s.GuildMemberRoleAdd(
+				os.Getenv("DISCORD_GUILD_ID"),
+				i.Member.User.ID,
+				sess.DiscordRoleId,
+			)
+			if err != nil {
+				fmt.Println(err)
+				logs.Output(
+					logs.ERROR,
+					"Could set role to subscriber.",
+				)
+			}
+
+			_, err = s.FollowupMessageCreate(
+				i.Interaction,
+				false,
+				&discordgo.WebhookParams{
+					Content: fmt.Sprintf(
+						`Vous êtes inscrit à la session : **%s** - *%s*. Elle aura lieu le %s.`,
+						sess.Speaker, sess.Name, sess.Date,
+					),
+				},
+			)
+
+			if err != nil {
+				logs.Output(
+					logs.ERROR,
+					"Could not initiate bot response.",
+				)
+			}
+		}
+	}
+}
+
+func RegisterSubscriberToNewSession(discordId string, speaker string) (*models.Session, error) {
+	ctx := context.Background()
+
+	// Get subscriber
+	subscriber, err := repositories.GetSubscriberByDiscordId(discordId)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add subscriber to session
+	sess, _, err := services.SubscribeToSession(ctx, subscriber, speaker)
+	if err != nil {
+		return nil, err
+	}
+
+	return sess, nil
 }
