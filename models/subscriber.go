08d73c4ccb890172d242a023471f7edf23c8ded9
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-01-06 19:56:48 +0100

    [MOD] SubscriberWithChallenge + remove underscore to lastname/firstname properties

--- models/subscriber.go
+++ models/subscriber.go
@@ -2,12 +2,30 @@ package models
 
 type Subscriber struct {
 	Id            string `json:"id"`
-  Firstname     string `json:"first_name" binding:"required"`
-  Lastname      string `json:"last_name" binding:"required"`
+  Firstname     string `json:"firstname" binding:"required"`
+  Lastname      string `json:"lastname" binding:"required"`
   Email         string `json:"email" binding:"required"`
   Institution   string `json:"institution" binding:"required"`
 	EpitechDegree string `json:"epitech_degree"`
 	DiscordId     *string `json:"discord_id"`
+}
+
+type SubscriberWithChallenge struct {
+  Subscriber
+  Challenge
+  Altcha *string `json:"altcha" binding:"required"`
+}
+
+func FilterOutAltcha(m *SubscriberWithChallenge) Subscriber {
+  return Subscriber{
+    Id: m.Id,
+    Firstname: m.Firstname,
+    Lastname: m.Lastname,
+    Email: m.Email,
+    Institution: m.Institution,
+    EpitechDegree: m.EpitechDegree,
+    DiscordId: m.DiscordId,
+  }
 }
 
 func (m *Subscriber) GetID() string {
