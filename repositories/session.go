47ab54abf2979d1ab04e3f9d8ecb1cc8ace3a69f
myriam-epitech <90515848+myriam-epitech@users.noreply.github.com>
2024-01-07 10:22:17 +0100

    [MOD] prepare queries repositories session (#8)
    * [MOD] prepare queries repositories session
    
    * [MOD] rm useless functions
    
    ---------
    
    Co-authored-by: myriam-tmds <myriam@tmds.io>

--- repositories/session.go
+++ repositories/session.go
@@ -2,10 +2,8 @@ package repositories
 
 import (
 	"context"
-	"github.com/labstack/echo/v4"
-	"database/sql"
-	"errors"
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"numenv_subscription_api/db"
 	"numenv_subscription_api/errors/logs"
 	"numenv_subscription_api/models"
@@ -13,13 +11,13 @@ import (
 
 // Get the session metadata by the user unique string
 func GetSessionByUniqueStr(uniqueStr string) (*models.Session, error) {
-  client, err := db.Client()
-  if err != nil {
-    return nil, err
-  }
+	client, err := db.Client()
+	if err != nil {
+		return nil, err
+	}
 
-  sess := &models.Session{}
-  q := `SELECT 
+	sess := &models.Session{}
+	q := `SELECT 
     sessions.id, 
     name, 
     speaker, 
@@ -31,86 +29,55 @@ func GetSessionByUniqueStr(uniqueStr string) (*models.Session, error) {
     JOIN subscribers_to_sessions ON sessions.id = subscribers_to_sessions.sessions_id 
     WHERE subscribers_to_sessions.unique_str=$1`
 
-  err = client.
-    QueryRow(q, uniqueStr).
-    Scan(
-      &sess.Id,
-      &sess.Name,
-      &sess.Speaker,
-      &sess.Date,
-      &sess.Type,
-      &sess.DiscordRoleId,
-      &sess.NumSubscribers,
-    )
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      fmt.Sprintf(
-        "Error occurred when retrieving session by UniqueStr (%s). Query : %u\n",
-        uniqueStr,
-        err,
-      ),
-    )
-    return nil, err
-  }
-  return sess, nil
-}
+	stmt, err := client.Prepare(q)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			fmt.Sprintf(
+				"Could not prepare query. Query : %s.",
+				q,
+			),
+		)
+		return nil, err
+	}
 
-// Get the session metadata by session ID
-func GetSessionById(ctx context.Context, sessionId string) (*models.Session, error) {
-  dbClient, err := db.Client()
-  if err != nil {
-    return nil, err
-  }
-
-  sess := &models.Session{}
-  q := `SELECT 
-    id, 
-    name, 
-    speaker, 
-    date, 
-    type, 
-    discord_role_id, 
-    num_subscribers 
-    FROM sessions WHERE id=$1`
-
-  err = dbClient. 
-    QueryRowContext(ctx, q, sessionId). 
-    Scan(
-      &sess.Id, 
-      &sess.Name, 
-      &sess.Speaker, 
-      &sess.Date, 
-      &sess.Type, 
-      &sess.DiscordRoleId,
-      &sess.NumSubscribers,
-    )
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      fmt.Sprintf(
-        "Error occurred when retrieving session by Id (%s). Query : %u\n",
-        sessionId,
-        err,
-      ),
-    )
-    return nil, err
-  }
-  return sess, nil
+	err = stmt.
+		QueryRow(uniqueStr).
+		Scan(
+			&sess.Id,
+			&sess.Name,
+			&sess.Speaker,
+			&sess.Date,
+			&sess.Type,
+			&sess.DiscordRoleId,
+			&sess.NumSubscribers,
+		)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			fmt.Sprintf(
+				"Error occurred when retrieving session by UniqueStr (%s). Query : %u\n",
+				uniqueStr,
+				err,
+			),
+		)
+		return nil, err
+	}
+	return sess, nil
 }
 
 // Get the session metadata by speaker name
 func GetSessionBySpeaker(
-  ctx context.Context,
-  speaker string,
+	ctx context.Context,
+	speaker string,
 ) (*models.Session, error) {
-  dbClient, err := db.Client()
-  if err != nil {
-    return nil, err
-  }
+	dbClient, err := db.Client()
+	if err != nil {
+		return nil, err
+	}
 
-  sess := &models.Session{}
-  q := `SELECT 
+	sess := &models.Session{}
+	q := `SELECT 
     id, 
     name, 
     speaker, 
@@ -120,97 +87,92 @@ func GetSessionBySpeaker(
     num_subscribers 
     FROM sessions WHERE speaker=$1`
 
-  err = dbClient.
-    QueryRowContext(ctx, q, speaker).
-    Scan(
-      &sess.Id, 
-      &sess.Name, 
-      &sess.Speaker,
-      &sess.Date,
-      &sess.Type,
-      &sess.DiscordRoleId,
-      &sess.NumSubscribers,
-    )
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      fmt.Sprintf(
-        "Error occurred when retrieving session by speaker name (%s). Query : %u\n",
-        speaker,
-        err,
-      ),
-    )
-    return nil, err
-  }
-  return sess, nil
+	stmt, err := dbClient.Prepare(q)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			fmt.Sprintf(
+				"Could not prepare query. Query : %s.",
+				q,
+			),
+		)
+		return nil, err
+	}
+
+	err = stmt.
+		QueryRowContext(ctx, speaker).
+		Scan(
+			&sess.Id,
+			&sess.Name,
+			&sess.Speaker,
+			&sess.Date,
+			&sess.Type,
+			&sess.DiscordRoleId,
+			&sess.NumSubscribers,
+		)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			fmt.Sprintf(
+				"Error occurred when retrieving session by speaker name (%s). Query : %u\n",
+				speaker,
+				err,
+			),
+		)
+		return nil, err
+	}
+	return sess, nil
 }
 
 // Count the number of subscribers for a given session
 // queried by speaker name
 func GetSessionNumberSubscribersBySpeaker(
-  ctx echo.Context, 
+	ctx echo.Context,
 ) (*int, error) {
-  dbClient, err := db.Client()
-  if err != nil {
-    return nil, err
-  }
-
-  speaker := ctx.Param("speaker")
-
-  var count int
-  q := `SELECT COUNT(*) FROM subscribers_to_sessions 
-    JOIN sessions ON sessions.id = subscribers_to_sessions.sessions_id
-    WHERE sessions.speaker=$1`
-
-  err = dbClient.QueryRow(q, speaker).Scan(&count)
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      "Could not get number of subscribers from database.",
-    )
-    return nil, err
-  }
-
-  return &count, nil
-}
-
-// Get the session metadata by session name
-func GetSessionByName(ctx context.Context, name string) (*models.Session, error) {
 	dbClient, err := db.Client()
 	if err != nil {
 		return nil, err
 	}
 
-	sess := &models.Session{}
-	q := "SELECT id, name, num_subscribers FROM sessions WHERE name=$1"
-	err = dbClient.
-    QueryRowContext(ctx, q, name).
-    Scan(&sess.Id, &sess.Name, &sess.NumSubscribers)
+	speaker := ctx.Param("speaker")
+
+	var count int
+	q := `SELECT COUNT(*) FROM subscribers_to_sessions 
+    JOIN sessions ON sessions.id = subscribers_to_sessions.sessions_id
+    WHERE sessions.speaker=$1`
+
+	stmt, err := dbClient.Prepare(q)
 	if err != nil {
 		logs.Output(
-      logs.ERROR, 
-      fmt.Sprintf(
-        "Error occurred with Get session by name (%s) query: %v\n", 
-        name, 
-        err,
-      ),
-    )
-		if errors.Is(err, sql.ErrNoRows) {
-			logs.Output(logs.WARNING, "no sessions found with that ID")
-			return nil, err
-		}
+			logs.ERROR,
+			fmt.Sprintf(
+				"Could not prepare query. Query : %s.",
+				q,
+			),
+		)
 		return nil, err
 	}
 
-	return sess, nil
+	err = stmt.QueryRow(speaker).Scan(&count)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			"Could not get number of subscribers from database.",
+		)
+		return nil, err
+	}
+
+	return &count, nil
 }
 
 func GetAllSessions() ([]*models.Session, error) {
-  client, err := db.Client()
-  if err != nil { return nil, err }
-  defer client.Close()
+	client, err := db.Client()
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
 
-  q := `SELECT 
+	q := `SELECT 
     id, 
     name, 
     speaker, 
@@ -220,52 +182,52 @@ func GetAllSessions() ([]*models.Session, error) {
     discord_role_id 
     FROM sessions`
 
-  stmt, err := client.Prepare(q)
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      fmt.Sprintf(
-        "Could not prepare query. Query : %s.",
-        q,
-      ),
-    )
-    return nil, err
-  }
+	stmt, err := client.Prepare(q)
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			fmt.Sprintf(
+				"Could not prepare query. Query : %s.",
+				q,
+			),
+		)
+		return nil, err
+	}
 
-  rows, err := stmt.Query()
-  if err != nil {
-    logs.Output(
-      logs.ERROR,
-      fmt.Sprintf(
-        "Could not execute the query. Query : %s, produced error : %s.",
-        q,
-        err,
-      ),
-    )
-  }
+	rows, err := stmt.Query()
+	if err != nil {
+		logs.Output(
+			logs.ERROR,
+			fmt.Sprintf(
+				"Could not execute the query. Query : %s, produced error : %s.",
+				q,
+				err,
+			),
+		)
+	}
 
-  var result []*models.Session
-  for rows.Next() {
-    var session models.Session
+	var result []*models.Session
+	for rows.Next() {
+		var session models.Session
 
-    err := rows.Scan(
-      &session.Id,
-      &session.Name,
-      &session.Speaker,
-      &session.Type,
-      &session.Date,
-      &session.NumSubscribers,
-      &session.DiscordRoleId,
-    )
-    if err != nil {
-      logs.Output(
-        logs.ERROR,
-        "Values retrieved from database did not match model properties.",
-      )
-      return nil, err
-    }
-    result = append(result, &session)
-  }
+		err := rows.Scan(
+			&session.Id,
+			&session.Name,
+			&session.Speaker,
+			&session.Type,
+			&session.Date,
+			&session.NumSubscribers,
+			&session.DiscordRoleId,
+		)
+		if err != nil {
+			logs.Output(
+				logs.ERROR,
+				"Values retrieved from database did not match model properties.",
+			)
+			return nil, err
+		}
+		result = append(result, &session)
+	}
 
-  return result, nil
+	return result, nil
 }
